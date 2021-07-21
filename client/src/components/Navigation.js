20d51bfd336b65ab8f5e0e0ebceaef0af5ad5c85
multicampus <rladmswlek@naver.com>
2021-07-21 16:44:56 +0900

    Modify nav feature

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,42 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation() {
   return (
     <div className="container">
       <div className="logo">
-        <Link to="/">
+        <NavLink to="/">
           <button>우리로고</button>
-        </Link>
+        </NavLink>
       </div>
       <div className="content">
         <div className="categories">
           <div className="mypage category">
-            <Link to="/mypage">
+            <NavLink to="/mypage" activeClassName="selected">
               <button className="category-btn">마이페이지</button>
-            </Link>
+            </NavLink>
           </div>
           <div className="search-friends category">
-            <Link to="/search-friends">
+            <NavLink to="/search-friends" activeClassName="selected">
               <button className="category-btn">친구찾기</button>
-            </Link>
+            </NavLink>
           </div>
           <div className="articles category">
-            <Link to="/articles">
+            <NavLink to="/articles" activeClassName="selected">
               <button className="category-btn">건의게시판</button>
-            </Link>
+            </NavLink>
           </div>
           <div className="setting category">
-            <Link to="/setting">
+            <NavLink to="/setting" activeClassName="selected">
               <button className="category-btn">환경설정</button>
-            </Link>
+            </NavLink>
           </div>
         </div>       
         <div className="logout">
-          <Link to="/logout">
+          <NavLink to="/logout">
             <button className="logout-btn">로그아웃</button>
-          </Link>
+          </NavLink>
         </div> 
       </div>     
     </div>
