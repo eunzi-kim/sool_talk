00d98afee92f2a7bd3d9217cba5206743c5f2117
rladmswlek <rladmswlek@naver.com>
2021-08-04 17:12:00 +0900

    Add hamburger feature

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import "./Navigation.css";
+import Burger from "./Burger";
+import "./css/Navigation.css";
 
 function Navigation() {
   const Logout = () => {
@@ -8,42 +9,48 @@ function Navigation() {
   }
 
   return (
-    <div className="nav-container">
-      <div className="logo">
-        <NavLink to="/">
-          <button>우리로고</button>
-        </NavLink>
-      </div>
-      <div className="content">
-        <div className="categories">
-          <div className="mypage category">
-            <NavLink to="/mypage" activeClassName="selected">
-              <button className="category-btn">마이페이지</button>
-            </NavLink>
-          </div>
-          <div className="search-friends category">
-            <NavLink to="/search-friends" activeClassName="selected">
-              <button className="category-btn">친구찾기</button>
-            </NavLink>
-          </div>
-          <div className="articles category">
-            <NavLink to="/articles" activeClassName="selected">
-              <button className="category-btn">게시판</button>
-            </NavLink>
-          </div>
-          <div className="setting category">
-            <NavLink to="/setting" activeClassName="selected">
-              <button className="category-btn">환경설정</button>
-            </NavLink>
-          </div>
-        </div>       
-        <div className="logout">
-          <NavLink to="/login">
-            <button onClick={Logout} className="logout-btn">로그아웃</button>
+    <div>
+      <div className="nav-container">
+        <div className="logo">
+          <NavLink to="/">
+            <button>우리로고</button>
           </NavLink>
+        </div>
+        <div className="content">
+          <div className="categories">
+            <div className="mypage category">
+              <NavLink to="/mypage" activeClassName="selected">
+                <button className="category-btn">마이페이지</button>
+              </NavLink>
+            </div>
+            <div className="search-friends category">
+              <NavLink to="/search-friends" activeClassName="selected">
+                <button className="category-btn">친구찾기</button>
+              </NavLink>
+            </div>
+            <div className="articles category">
+              <NavLink to="/articles" activeClassName="selected">
+                <button className="category-btn">게시판</button>
+              </NavLink>
+            </div>
+            <div className="setting category">
+              <NavLink to="/setting" activeClassName="selected">
+                <button className="category-btn">환경설정</button>
+              </NavLink>
+            </div>
+          </div>       
+          <div className="logout">
+            <NavLink to="/login">
+              <button onClick={Logout} className="logout-btn">로그아웃</button>
+            </NavLink>
+          </div> 
         </div> 
-      </div>     
+      </div>
+      <div className="hamburger">
+        <Burger />    
+      </div>
     </div>
+    
   );
 }
 
