f83b3c7d4a1644245c4b4d8a2896fcc880afeaba
PC2 <sjd0051@naver.com>
2021-08-11 13:54:18 +0900

    message

---
+++ server/src/main/java/talk/server/dao/BoardDao.java
@@ -0,0 +1,23 @@
+package talk.server.dao;
+
+import talk.server.vo.Board;
+
+import java.util.ArrayList;
+
+public interface BoardDao {
+    // 게시글 정보 불러오기
+    public ArrayList<Board> getAllBoard();
+
+    // 게시글 CRUD
+    //C
+    public int createsuc(Board board);
+
+    // 게시글 디테일 정보 불러오기
+    public Board boarddetail(int board_no);
+
+    // 게시글 업데이트
+    public int boardupdate(Board board);
+
+    //게시글 삭제
+    public void boarddelete(int board_no);
+}
